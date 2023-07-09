6dbd8264db154a71d262dc51d262878a37ba6f3e
Nikita <reichert.nikita@gmail.com>
2023-07-09 14:18:31 +0200

    added ngOnInit

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Car } from './cars';
 
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.component.html',
-  styleUrls: ['./cars.component.scss']
+  styleUrls: ['./cars.component.scss'],
 })
-export class CarsComponent {
+export class CarsComponent implements OnInit {
+  cars: Car[] = [];
 
+  ngOnInit(): void {
+    this.cars = [
+      { brand: 'Porsche', model: '991' },
+
+      { brand: 'Porsche', model: '991' },
+    ];
+  }
 }
---
+++ src/app/cars/cars.ts
@@ -0,0 +1,8 @@
+export interface Scheme {
+  quantity: string;
+}
+
+export interface Car {
+  brand: string;
+  model: string;
+}
