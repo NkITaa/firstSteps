5d3cc455bdfe7988eb230aba7389f8affecab200
Nikita <reichert.nikita@gmail.com>
2023-07-10 08:54:22 +0200

    created service

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.scss']
+  styleUrls: ['./employees.component.scss'],
 })
 export class EmployeesComponent {
-
+  empName: string = 'Nikita';
 }
