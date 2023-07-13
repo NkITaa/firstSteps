d635fd9bc03d190fe31d25a2a5bc833129c8c135
Nikita <reichert.nikita@gmail.com>
2023-07-13 18:19:12 +0200

    booking history basic mat form field

--- src/app/booking-history/booking-history.component.ts
+++ src/app/booking-history/booking-history.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ConfigService } from '../services/config.service';
 @Component({
   selector: 'app-booking-history',
   templateUrl: './booking-history.component.html',
-  styleUrls: ['./booking-history.component.scss']
+  styleUrls: ['./booking-history.component.scss'],
 })
-export class BookingHistoryComponent {
+export class BookingHistoryComponent implements OnInit {
+  bookingForm!: FormGroup;
 
+  get guests() {
+    return this.bookingForm.get('guests') as FormArray;
+  }
+
+  constructor(private configService: ConfigService, private fb: FormBuilder) {}
+
+  // Form could be designed like this -> has the advantage that the info is correctly formatted as json
+  ngOnInit(): void {
+    this.bookingForm = this.fb.group({
+      bookingId: ['', Validators.required, Validators.email],
+      roomId: new FormControl(
+        { value: '2', disabled: true },
+        { validators: [Validators.email] }
+      ),
+      guestEmail: [''],
+      checkinDate: [''],
+      checkoutDate: [''],
+      bookingStatus: [''],
+      bookingAmmount: [''],
+      bookingDate: [''],
+      mobileNumber: [''],
+      guestName: [''],
+      guestAdress: this.fb.group({
+        guestCity: [''],
+        guestState: [''],
+        guestCountry: [''],
+        guestZipCode: [''],
+      }),
+      guests: this.fb.array([1, 23, 2]),
+    });
+  }
+
+  addGuest() {
+    this.guests.push(this.fb.array([1, 23, 2]));
+  }
+
+  // so kann man neue Control zu dem Json oben hinzufügen
+  addPassport() {
+    this.bookingForm.addControl('passport', new FormControl(''));
+  }
+
+  removePassport() {
+    if (this.bookingForm.get('passport')) {
+      this.bookingForm.removeControl('passport');
+    }
+  }
+
+  removeGuest(i: number) {
+    this.guests.removeAt(i);
+  }
 }
--- src/app/booking-history/booking-history.module.ts
+++ src/app/booking-history/booking-history.module.ts
@@ -3,15 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { BookingHistoryRoutingModule } from './booking-history-routing.module';
 import { BookingHistoryComponent } from './booking-history.component';
-
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
 @NgModule({
-  declarations: [
-    BookingHistoryComponent
-  ],
+  declarations: [BookingHistoryComponent],
   imports: [
     CommonModule,
-    BookingHistoryRoutingModule
-  ]
+    BookingHistoryRoutingModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatExpansionModule,
+  ],
 })
-export class BookingHistoryModule { }
+export class BookingHistoryModule {}
